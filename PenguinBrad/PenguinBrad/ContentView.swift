e3eead14acd5ea40d95ae4c9020326e3519cfef5
Fernando Lobo <fernandosclb@gmail.com>
2020-07-21 15:22:11 -0300

    Inicialização do projeto

---
+++ PenguinBrad/PenguinBrad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PenguinBrad
+//
+//  Created by Fernando Lobo on 21/07/20.
+//  Copyright © 2020 Fernando Lobo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
