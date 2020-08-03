bcf526d1713dc155ae6b7c67fb8a15886f46959c
Bruna Costa <brunafranca704@gmail.com>
2020-08-03 15:31:33 -0300

    challenges

--- PenguinBrad/PenguinBrad/Model/Challenges.swift
+++ PenguinBrad/PenguinBrad/Model/Challenges.swift
@@ -20,6 +20,51 @@ struct Challenges: View {
                 List(challengeData){ challenge in
                     VStack{
                         HStack(alignment: .top){
+                            Image(challenge.imageName)
+                            .resizable()
+                            .frame(width: 80, height: 90, alignment: .trailing)
+                                .overlay(Rectangle().stroke(Color.secondary, lineWidth: 1)).padding(.top, 10)
+                            VStack(alignment: .leading){
+                                Text(challenge.name).font(.system(size: 20, weight: .bold, design: .default)).minimumScaleFactor(0.5)
+                                Text(challenge.topic).font(.system(size: 16, weight: .semibold, design: .default)).minimumScaleFactor(0.5)
+                                    .foregroundColor(.secondary)
+                            }.navigationBarTitle("Challenges").font(.title).padding(.top, 10).padding(.leading, 5)
+                            Spacer()
+                        }
+                        NavigationLink(destination: Text("Instructions")) {
+                            EmptyView()
+                        }.buttonStyle(PlainButtonStyle())
+                    }
+                    
+                }
+                
+            }
+
+        }
+    }
+}
+
+
+struct Challenges_Previews: PreviewProvider {
+    static var previews: some View {
+        Challenges()
+    }
+}
+/*
+import SwiftUI
+
+
+struct Challenges: View {
+    
+    var body: some View {
+        
+        ZStack{
+            
+            NavigationView{
+                
+                List(challengeData){ challenge in
+                    VStack{
+                        HStack(alignment: .top){
                             VStack(alignment: .leading){
                                 Text(challenge.name).font(.system(size: 20, weight: .bold, design: .default))
                                 Text(challenge.topic).font(.system(size: 16, weight: .semibold, design: .default))
@@ -50,3 +95,4 @@ struct Challenges_Previews: PreviewProvider {
         Challenges()
     }
 }
+*/
